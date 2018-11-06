060755871aad680040246d4975ef8d6c07cefb43
kpcruz <kelvinmembership@gmail.com>
2018-11-06 02:49:22 -0800

    Toying with Inquirer as a class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,20 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const colors = require('colors');
 const config = require('./config');
-cTable = require('console.table');
+require('colors');
+require('console.table');
 
 const connection = mysql.createConnection(config);
 connection.connect(function (err) {
 	if (err) throw err;
-	console.log("connected as id " + connection.threadId + "\n");
-	view.displayTable();
+	view.displayApp();
 });
 
 var view = {
-	displayTable: function () {
+	displayApp: function () {
 		console.clear();
-		console.log("WELCOME TO THE BAMAZON STORE 1998" .magenta)
-		var sql = "SELECT * FROM products";
-		connection.query(sql, function (err, res) {
+		console.log("WELCOME TO THE BAMAZON STORE 1998".magenta)
+		connection.query("SELECT * FROM products", function (err, res) {
 			if (err) throw err;
 			console.table(res);
 			view.displayMenu();
@@ -29,8 +27,8 @@ var view = {
 				choices: ["buy", "sell", "update-price", "exit"],
 				name: "command"
 			}, ])
-			.then(function (inquirerResponse) {
-				var input = inquirerResponse.command
+			.then(function (response) {
+				var input = response.command
 				if (input === "buy") {
 					app.buy();
 				} else if (input === "sell") {
@@ -48,13 +46,8 @@ var query = {
 	runQuery: function (sql, data) {
 		connection.query(sql, data, function (err, res) {
 			if (err) throw err;
-			view.displayTable();
+			view.displayApp();
 		});
-	},
-	display: function (input) {
-		var sql = "SELECT * FROM products WHERE id = ?";
-		var data = [input];
-		query.runQuery(sql, data);
 	},
 	buy: function (id, input) {
 		var sql = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?";
@@ -73,69 +66,72 @@ var query = {
 	},
 };
 
-//Inquirer npm constructor
-function Inquirer(type, message, name) {
-	this.type = type;
-	this.message = message;
-	this.name = name;
-};
-Inquirer.prototype.printPrompt = function () {
-	return {
-		type: this.type,
-		message: "What item would you like to " + this.message + "?",
-		name: this.name,
-		validate: app.validate,
+//Create inquirer prompts
+class Inquirer {
+	constructor(message, name) {
+		this.message = message;
+		this.name = name;
 	}
+	promptID() {
+			return {
+				type: "input",
+				message: "What item would you like to " + `${this.message}` + "?",
+				name: "id",
+				validate: app.validateInput,
+			}
+		};
+	promptQTY() {
+			return {
+				type: "input",
+				message: "Input amount",
+				name: `${this.name}`,
+				validate: app.validateInput,
+			}
+		};
 };
-Inquirer.prototype.printQTY = function () {
-	return {
-		type: this.type,
-		message: "Input amount",
-		name: this.name,
-		validate: app.validate,
-	}
-};
+const inputBuy = new Inquirer('buy');
+const inputSell = new Inquirer('sell');
+const inputUpdate = new Inquirer('price update');
+const inputQTY = new Inquirer(undefined, 'qty');
+const inputPrice = new Inquirer(undefined, 'price');
 
 var app = {
-	validate: function (input) {
+	validateInput: function (input) {
 		if (input.length < 1) {
-			console.log("missing input" .red)
+			console.log("missing input".red)
 			return false;
 		} else {
 			return true;
 		}
 	},
 	buy: function () {
-		var inputID = new Inquirer("input", "buy", "id");
-		var inputQTY = new Inquirer("input", "buy", "qty");
 		inquirer.prompt([
-				inputID.printPrompt(),
-				inputQTY.printQTY(),
+			inputBuy.promptID(),
+			inputQTY.promptQTY(),
 			])
 			.then(function (data) {
 				query.buy(data.id, data.qty);
 			})
 	},
 	sell: function () {
-		var inputID = new Inquirer("input", "sell", "id");
-		var inputQTY = new Inquirer("input", "sell", "qty");
 		inquirer.prompt([
-				inputID.printPrompt(),
-				inputQTY.printQTY(),
+			inputSell.promptID(),
+			inputQTY.promptQTY(),
 			])
 			.then(function (data) {
 				query.sell(data.id, data.qty);
 			})
 	},
 	updatePrice: function () {
-		var inputID = new Inquirer("input", "price update", "id");
-		var inputQTY = new Inquirer("input", "update", "price");
 		inquirer.prompt([
-				inputID.printPrompt(),
-				inputQTY.printQTY(),
+			inputUpdate.promptID(),
+			inputPrice.promptQTY(),
 			])
 			.then(function (data) {
 				query.updatePrice(data.id, data.price);
 			})
 	},
 };
+
+
+
