2788c20a6c233babe485524ebc6b1ec8c068e611
kpcruz <kelvinmembership@gmail.com>
2022-11-27 13:56:53 -0800

    fixed mysql localhost connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import connection from "./database/connection";
 import globalUI from "./views/globalUI";
 
-connection.connect(function(err) {
-  if (err) throw err;
-  globalUI();
+connection.connect(function (err) {
+	if (err) throw err;
+	globalUI();
 });
